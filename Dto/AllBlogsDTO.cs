94844c4bea9083be2cdd328621724eb8c2fbb2fb
zezotaha1 <zezo.taha12345@gmail.com>
2024-05-05 14:23:22 +0300

    add AllBlogsDTO :Dsuke

---
+++ Dto/AllBlogsDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BugDetectorGP.Dto
+{
+    public class AllBlogsDTO
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string UsrName { get; set; }
+        public int NumberOfLikes { get; set; }
+        public int NumberOfDisLikes { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+    }
+}
