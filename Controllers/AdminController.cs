725f9405edb97569332869ba30de675603fde795
Mahmoud Mohamed Ahmed <91031578+Mahmoud-desuky@users.noreply.github.com>
2024-05-08 17:26:58 +0300

    Create AdminController.cs
    Admin Controller :Desouky

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,69 @@
+﻿using BugDetectorGP.Dto;
+using BugDetectorGP.Models;
+using BugDetectorGP.Models.blog;
+using BugDetectorGP.Models.user;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugDetectorGP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize("Admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly ApplicationDbContext _Context;
+        public AdminController(ApplicationDbContext Context)
+        {
+            _Context = Context;
+        }
+        [HttpPost("AddBlog")]
+        public async Task<IActionResult> AddBlog(BlogModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var NewBlog = new Blogs();
+            NewBlog.Title = model.Title;
+            NewBlog.Content = model.Content;
+            NewBlog.PublicationDate = DateTime.UtcNow.ToLocalTime();
+            NewBlog.UserId = "1";
+            _Context.Blogs.Add(NewBlog);
+            _Context.SaveChanges();
+            return Ok("New Blog a");
+         }
+        [HttpDelete("DeleteBlog")]
+        public async Task<IActionResult> DeleteBlog(BlogLikeAndDisLikeDTO model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var blog = await _Context.Blogs.SingleOrDefaultAsync(u => u.BlogId == model.Blogid);
+            if (blog == null)
+                return BadRequest("Blog Not Found");
+         
+                _Context.Blogs.Remove(blog);
+                _Context.SaveChanges();
+                return Ok("Blog are Deleted");
+        }
+        [HttpDelete("DeleteComment")]
+        public async Task<IActionResult> DeleteComment(DeleteCommentDto model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var blog = await _Context.Blogs.SingleOrDefaultAsync(u => u.BlogId == model.BlogId);
+            var comment = await _Context.Comment.SingleOrDefaultAsync(c => c.CommentId == model.CommentId);
+            if (blog == null || comment == null)
+                return BadRequest("Blog or Comment Not Found");
+            _Context.Comment.Remove(comment);
+            _Context.SaveChanges();
+            return Ok("Comment are Deleted");
+        }
+
+    }
+}
